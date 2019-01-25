1b05fdfc3654d63bfd2b0936601d3c01be9bd6a7
Ivan Putra Eriansya <putra.ivan90@gmail.com>
2019-01-25 15:40:21 +0700

    Add unit test for error page

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import urllib
+
 from flask import abort, url_for
 from flask_testing import TestCase
 
@@ -178,6 +180,35 @@ class TestViews(TestBase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, redirect_url)
 
+
+class TestErrorPage(TestBase):
+
+    def test_403_forbidden(self):
+        # create route to abort the request with the 403 error
+        @self.app.route('/403')
+        def forbidden_error():
+            abort(403)
+
+        response = self.client.get('/403')
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue("403 Error" in response.data)
+
+    
+    def test_404_not_found(self):
+        response = self.client.get('/nothinghere')
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue("404 Error" in response.data)
+
+
+    def test_500_internal_server_error(self):
+        # create route to abort the request with the 500 error
+        @self.app.route('/500')
+        def internal_server_error():
+            abort(500)
+
+        response = self.client.get('/500')
+        self.assertEqual(response.status_code, 500)
+        self.assertTrue("500 Error" in response.data)
 
 if __name__ == '__main__':
     unittest.main()
