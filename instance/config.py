4cda94efc213e839aa6ebd1c5a69dc5b3d870d28
Ivan Putra Eriansya <putra.ivan90@gmail.com>
2019-01-26 11:26:55 +0700

    Fix invalid escape sequence of /M

--- instance/config.py
+++ instance/config.py
@@ -5,5 +5,5 @@ SECRET_KEY = 'p9Bv<3Eid9%$i01'
 # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://DRIVER={SQL Server Native Client 11.0};SERVER=(localdb)\MSSQLLocalDB;DATABASE=ElectronicBookBuilding;Trusted_Connection=yes'
 # SQLALCHEMY_DATABASE_URI = 'mssql://(localdb)\MSSQLLocalDB/ElectronicBookBuilding?trusted_connection=yes'
 
-params = urllib.parse.quote_plus('DRIVER={SQL Server Native Client 11.0};SERVER=(localdb)\MSSQLLocalDB;DATABASE=dreamteam_db;Trusted_Connection=yes;')
+params = urllib.parse.quote_plus(r'DRIVER={SQL Server Native Client 11.0};SERVER=(localdb)\MSSQLLocalDB;DATABASE=dreamteam_db;Trusted_Connection=yes;')
 SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % params
